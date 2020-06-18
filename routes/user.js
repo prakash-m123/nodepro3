53e2bbc8c90111adaf4eb9ebc0f150c74c9c5d5a
prakash-m123 <prakash@spericorn.com>
2020-06-18 15:03:05 +0530

    changed

--- routes/user.js
+++ routes/user.js
@@ -7,27 +7,7 @@ const userController= require('../controllers/user');
 
 const router = express.Router();
 
-router.put('/register',
-[
- body('phone')
- .trim()
- .isLength({ min: 10 }),
- body('email')
-      .isEmail()
-      .withMessage('Please enter a valid email.')
-      .custom((value, { req }) => {
-        return User.findOne({ email: value }).then(userDoc => {
-          if (userDoc) {
-            return Promise.reject('E-Mail address already exists!');
-          }
-        });
-      })
-      .normalizeEmail(),
- body('password')
- .trim()
- .isLength({ min: 5 })
-],
-userController.userreg );
+router.put('/register',userController.userreg );
 
 
 module.exports = router;
