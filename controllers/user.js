e88a0c60523962e07577cf1be446425bce77eb68
prakash-m123 <prakash@spericorn.com>
2020-06-18 17:30:01 +0530

    user changed

--- controllers/user.js
+++ controllers/user.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
-exports.userreg = (req, res, next) => {
+exports.userreg = (req,res,next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       const error = new Error('Validation failed.');
@@ -13,28 +13,27 @@ exports.userreg = (req, res, next) => {
       error.data = errors.array();
       throw error;
     }
-    const firstname = req.body.firstname;
-    const lastname = req.body.lastname;
-    const phone = req.body.phone;
-    const email = req.body.email;
-    const password = req.body.password;
+    const firstname=req.body.firstname;
+    const lastname=req.body.lastname;
+    const phone=req.body.phone;
+    const email=req.body.email;
+    const password=req.body.password;
     bcrypt
-      .hash(password, 12)
+      .hash(password,12)
       .then(hashedPass => {
-        const user = new User({
-          firstname: firstname,
-          lastname: lastname,
-          phone: phone,
-          email: email,
-          password: hashedPass,
+        const user=new User({
+          firstname:firstname,
+          lastname:lastname,
+          phone:phone,
+          email:email,
+          password:hashedPass,
           
         });
         return user.save();
-        
       })
       
       .then(result => {
-        res.status(201).json({ message: 'User registered!', userId: result._id });
+        res.status(201).json({ message: 'User registered!', userId:result._id });
       })
      
       .catch(err => {
